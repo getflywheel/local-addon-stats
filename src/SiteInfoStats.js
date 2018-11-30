f426ef42e46b0327e259163a5812a6e4837e6275
Clay Griffiths <clay@claygriffiths.com>
2018-11-30 16:40:08 -0600

    style: add ESLint rules and run fix

--- src/SiteInfoStats.js
+++ src/SiteInfoStats.js
@@ -1,7 +1,7 @@
-const {SmoothieChart, TimeSeries} = require('smoothie');
+const { SmoothieChart, TimeSeries } = require('smoothie');
 const childProcess = require('child_process');
 const readline = require('readline');
-const debounce = require('lodash/debounce');
+const debounce = require('lodash.debounce');
 
 module.exports = function (context) {
 
@@ -10,15 +10,15 @@ module.exports = function (context) {
 	const $ = context.jQuery;
 
 	const timestampFormatter = (date) => {
-		function pad2(number) {
-			return (number < 10 ? '0' : '') + number
+		function pad2 (number) {
+			return (number < 10 ? '0' : '') + number;
 		}
 
 		return pad2(date.getHours()) + ':' + pad2(date.getMinutes()) + ':' + pad2(date.getSeconds());
 	};
 
 	return class SiteInfoStats extends Component {
-		constructor(props) {
+		constructor (props) {
 			super(props);
 
 			this.statsProcess = undefined;
@@ -27,11 +27,11 @@ module.exports = function (context) {
 
 			this.state = {
 				cpu: null,
-				memory: null
-			}
+				memory: null,
+			};
 		}
 
-		componentDidMount() {
+		componentDidMount () {
 
 			this.startCPUChart();
 			this.startMemoryChart();
@@ -40,16 +40,16 @@ module.exports = function (context) {
 
 		}
 
-		getDockerStats() {
+		getDockerStats () {
 
-			let siteID = this.props.params.siteID;
-			let site = this.props.sites[siteID];
+			const siteID = this.props.params.siteID;
+			const site = this.props.sites[siteID];
 
-			this.statsProcess = childProcess.spawn(context.environment.dockerPath, ['stats', site.container], {env: context.environment.dockerEnv});
+			this.statsProcess = childProcess.spawn(context.environment.dockerPath, ['stats', site.container], { env: context.environment.dockerEnv });
 
 			readline.createInterface({
 				input: this.statsProcess.stdout,
-				terminal: false
+				terminal: false,
 			}).on('line', debounce((line) => {
 
 				if (!this.memorySeries || !this.cpuSeries) {
@@ -58,23 +58,23 @@ module.exports = function (context) {
 
 				if (line.indexOf(site.container) !== -1) {
 
-					let matches = (/^[a-z0-9]+\s+([0-9.%]+)\s+([0-9.]+\s*MiB)/gmi).exec(line);
+					const matches = (/^[a-z0-9]+\s+([0-9.%]+)\s+([0-9.]+\s*MiB)/gmi).exec(line);
 
-					if ( !matches ) {
+					if (!matches) {
 						return;
 					}
-					
-					let time = new Date().getTime();
 
-					let cpuResult = matches[1].replace('%', '');
-					let memoryResult = matches[2].replace(/\s*MiB/, '');
+					const time = new Date().getTime();
 
-					this.cpuSeries.append(time, cpuResult);
-					this.memorySeries.append(time, matches[2].replace(/\s*MiB/, ''));
+					const cpuResult = matches[1].replace('%', '');
+					const memoryResult = matches[2].replace(/\s*MiB/, '');
+
+					this.cpuSeries.append(time, parseFloat(cpuResult));
+					this.memorySeries.append(time, parseFloat(memoryResult));
 
 					this.setState({
 						cpu: cpuResult,
-						memory: memoryResult
+						memory: memoryResult,
 					});
 
 				}
@@ -83,12 +83,12 @@ module.exports = function (context) {
 
 		}
 
-		startCPUChart() {
+		startCPUChart () {
 
 			$(this.refs['chart-cpu']).attr('width', $(this.refs['chart-cpu-container']).width());
 			$(this.refs['chart-cpu']).attr('height', $(this.refs['chart-cpu-container']).height() - 50);
 
-			var chart = new SmoothieChart({
+			const chart = new SmoothieChart({
 				millisPerPixel: 100,
 				interpolation: 'linear',
 				grid: {
@@ -96,7 +96,7 @@ module.exports = function (context) {
 					verticalSections: 5,
 					fillStyle: 'transparent',
 					borderVisible: false,
-					millisPerLine: 10000
+					millisPerLine: 10000,
 				},
 				maxValue: 100,
 				minValue: 0,
@@ -107,27 +107,27 @@ module.exports = function (context) {
 					return parseFloat(max).toFixed(precision) + '%';
 				},
 				labels: {
-					fillStyle: '#000000'
+					fillStyle: '#000000',
 				},
-				timestampFormatter: timestampFormatter
+				timestampFormatter: timestampFormatter,
 			});
 
 			this.cpuSeries = new TimeSeries();
 
-			chart.addTimeSeries(this.cpuSeries, {lineWidth: 1, strokeStyle: '#51bb7b'});
+			chart.addTimeSeries(this.cpuSeries, { lineWidth: 1, strokeStyle: '#51bb7b' });
 			chart.streamTo(this.refs['chart-cpu'], 1000);
 
-		};
+		}
 
-		startMemoryChart() {
+		startMemoryChart () {
 
-			let siteID = this.props.params.siteID;
-			let site = this.props.sites[siteID];
+			const siteID = this.props.params.siteID;
+			const site = this.props.sites[siteID];
 
 			$(this.refs['chart-memory']).attr('width', $(this.refs['chart-memory-container']).width());
 			$(this.refs['chart-memory']).attr('height', $(this.refs['chart-memory-container']).height() - 50);
 
-			var chart = new SmoothieChart({
+			const chart = new SmoothieChart({
 				millisPerPixel: 100,
 				interpolation: 'linear',
 				grid: {
@@ -135,7 +135,7 @@ module.exports = function (context) {
 					verticalSections: 5,
 					fillStyle: 'transparent',
 					borderVisible: false,
-					millisPerLine: 10000
+					millisPerLine: 10000,
 				},
 				yMinFormatter: function (min, precision) {
 					return parseFloat(min).toFixed(precision) + ' MiB';
@@ -144,18 +144,18 @@ module.exports = function (context) {
 					return parseFloat(max).toFixed(precision) + ' MiB';
 				},
 				labels: {
-					fillStyle: '#000000'
+					fillStyle: '#000000',
 				},
-				timestampFormatter: timestampFormatter
+				timestampFormatter: timestampFormatter,
 			});
 
 			this.memorySeries = new TimeSeries();
 
-			chart.addTimeSeries(this.memorySeries, {lineWidth: 1, strokeStyle: '#51bb7b'});
+			chart.addTimeSeries(this.memorySeries, { lineWidth: 1, strokeStyle: '#51bb7b' });
 			chart.streamTo(this.refs['chart-memory'], 1000);
 
-			childProcess.execFile(context.environment.dockerPath, ['stats', '--no-stream', site.container], {env: context.environment.dockerEnv}, (error, stdout, stderr) => {
-				let maxMemoryMatch = (/\/ ([0-9.]+\s*MiB)/gmi).exec(stdout);
+			childProcess.execFile(context.environment.dockerPath, ['stats', '--no-stream', site.container], { env: context.environment.dockerEnv }, (error, stdout, stderr) => {
+				const maxMemoryMatch = (/\/ ([0-9.]+\s*MiB)/gmi).exec(stdout);
 
 				chart.options.maxValue = parseInt(maxMemoryMatch[1].replace(/\s*MiB/, ''));
 				chart.options.minValue = 0;
@@ -164,9 +164,9 @@ module.exports = function (context) {
 
 		}
 
-		componentWillUnmount() {
+		componentWillUnmount () {
 
-			if ( this.statsProcess ) {
+			if (this.statsProcess) {
 
 				this.statsProcess.stdin.pause();
 				this.statsProcess.kill();
@@ -177,22 +177,22 @@ module.exports = function (context) {
 
 		}
 
-		render() {
+		render () {
 
 			return (
-				<div style={{display: 'flex', flexDirection: 'column', flex: 1, padding: '15px 0'}}>
-					<div style={{height: '50%', boxSizing: 'border-box', padding: '15px 0'}} ref="chart-cpu-container">
-						<h4 className="padded-horizontally-more" style={{margin: '0 30px 15px'}}>
+				<div style={{ display: 'flex', flexDirection: 'column', flex: 1, padding: '15px 0' }}>
+					<div style={{ height: '50%', boxSizing: 'border-box', padding: '15px 0' }} ref="chart-cpu-container">
+						<h4 className="padded-horizontally-more" style={{ margin: '0 30px 15px' }}>
 							CPU Usage
-							<span style={{float: 'right', fontFamily: 'monospace', color: '#51bb7b'}}>{this.state.cpu ? this.state.cpu + '%' : ''}</span>
+							<span style={{ float: 'right', fontFamily: 'monospace', color: '#51bb7b' }}>{this.state.cpu ? this.state.cpu + '%' : ''}</span>
 						</h4>
 
 						<canvas width="" height="" ref="chart-cpu"></canvas>
 					</div>
-					<div style={{height: '50%', boxSizing: 'border-box', padding: '0'}} ref="chart-memory-container">
-						<h4 className="padded-horizontally-more" style={{margin: '0 30px 15px'}}>
+					<div style={{ height: '50%', boxSizing: 'border-box', padding: '0' }} ref="chart-memory-container">
+						<h4 className="padded-horizontally-more" style={{ margin: '0 30px 15px' }}>
 							Memory Usage
-							<span style={{float: 'right', fontFamily: 'monospace', color: '#51bb7b'}}>{this.state.memory ? this.state.memory + ' MiB' : ''}</span>
+							<span style={{ float: 'right', fontFamily: 'monospace', color: '#51bb7b' }}>{this.state.memory ? this.state.memory + ' MiB' : ''}</span>
 						</h4>
 
 						<canvas width="400" height="200" ref="chart-memory"></canvas>
@@ -200,6 +200,6 @@ module.exports = function (context) {
 				</div>
 			);
 		}
-	}
+	};
 
-}
+};
--- src/renderer.js
+++ src/renderer.js
@@ -2,26 +2,24 @@
 
 const path = require('path');
 
-module.exports = function(context) {
+module.exports = function (context) {
 
 	const hooks = context.hooks;
 	const React = context.React;
-	const {Link, Route} = context.ReactRouter;
+	const { Link, Route } = context.ReactRouter;
 
 	const SiteInfoStats = require('./SiteInfoStats')(context);
 
-	hooks.addContent('routesSiteInfo', () => {
-		return <Route key="site-info-stats" path="/site-info/:siteID/stats" component={SiteInfoStats}/>
-	});
-	
-	hooks.addFilter('siteInfoMoreMenu', function(menu, site) {
-		
+	hooks.addContent('routesSiteInfo', () => <Route key="site-info-stats" path="/site-info/:siteID/stats" component={SiteInfoStats}/>);
+
+	hooks.addFilter('siteInfoMoreMenu', function (menu, site) {
+
 		menu.push({
 			label: 'Stats',
 			enabled: !this.context.router.isActive(`/site-info/${site.id}/stats`),
 			click: () => {
 				context.events.send('goToRoute', `/site-info/${site.id}/stats`);
-			}
+			},
 		});
 
 		return menu;
